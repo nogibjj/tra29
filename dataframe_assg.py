826c359becdc994d251a36d987b994e242a80b90
titusrobin <your.email@example.com>
2023-09-06 16:06:15 -0400

    "test-commit_week1"

---
+++ dataframe_assg.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# Sample data for the DataFrame
+data = {'Name': ['Alice', 'Bob', 'Charlie', 'David'],
+        'Age': [25, 30, 35, 40],
+        'City': ['New York', 'Los Angeles', 'Chicago', 'Houston']}
+
+# Create a DataFrame from the sample data
+df = pd.DataFrame(data)
+
+# Print the entire DataFrame
+print(df)
